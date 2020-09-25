c5f37ea826967da450961e0c2e1951ccc025c95f
Wesley de Nooijer <wesleydenooijer@gmail.com>
2020-09-25 19:45:56 +0200

    Clean up, links open new tab

--- script.js
+++ script.js
@@ -12,20 +12,20 @@ class navbar extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
     <div class="topnav" id="myTopnav">
-    <a href="wesleynooijer.github.io">Wesley de Nooijer</a>
+    <a href="../wesleynooijer.github.io">Wesley de Nooijer</a>
     <a href="essays-index.html">Essays</a>
     <a href="projects-index.html">Projects</a>
-    <a href="innovation-index.html">Roam</a>
-    <a class="social" aria-hidden="true" href="https://www.linkedin.com/in/wesleydenooijer/">
+    <a href="https://roamresearch.com/#/app/roamNERD_test/page/dfr2yR2Zs" target="_blank">Roam</a>
+    <a class="social" aria-hidden="true" href="https://www.linkedin.com/in/wesleydenooijer/" target="_blank">
       <i class="fa fa-linkedin" style="color:#0077B5"></i> 
     </a>
-    <a class="social" aria-hidden="true" href="https://medium.com/@wesleydenooijer">
+    <a class="social" aria-hidden="true" href="https://medium.com/@wesleydenooijer" target="_blank">
       <i class="fa fa-medium" style="color:white"></i> 
     </a>
-    <a class="social" aria-hidden="true" href="https://github.com/WesleyNooijer">
+    <a class="social" aria-hidden="true" href="https://github.com/WesleyNooijer" target="_blank">
       <i class="fa fa-github" style="color:white"></i> 
     </a>
-    <a class="social" aria-hidden="true" href="https://twitter.com/WesleyNooijer">
+    <a class="social" aria-hidden="true" href="https://twitter.com/WesleyNooijer" target="_blank">
       <i class="fa fa-twitter" style="color:#1DA1F2"></i> 
     </a>
     <a href="javascript:void(0);" class="icon" onclick="myFunction()">
